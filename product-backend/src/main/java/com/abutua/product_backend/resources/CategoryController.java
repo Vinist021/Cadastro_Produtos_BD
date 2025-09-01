8cad56f50357040caa56af9a527d5965e6f87b44
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-01 16:55:01 -0300

    Categorias adquiridas através do banco de dados

--- product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
@@ -1,41 +1,25 @@
 package com.abutua.product_backend.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.product_backend.models.Category;
+import com.abutua.product_backend.repositories.CategoryRepository;
 
 @CrossOrigin
 @RestController
 public class CategoryController {
-    
-    private List<Category> categories = Arrays.asList(  new Category(1, "Produção Própria"),
-                                                        new Category(2, "Nacional"),
-                                                        new Category(3, "Importado"),
-                                                        new Category(4, "Premium")
-    );
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+    
     @GetMapping("categories")
     public List<Category> getCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
-    @GetMapping("categories/{id}")
-    public ResponseEntity<Category> getCategoria(@PathVariable int id) {
-    
-        Category cat = categories.stream()
-                                .filter(p -> p.getId() == id)
-                                .findFirst()
-                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
-
-        return ResponseEntity.ok(cat);
-    }
 }
