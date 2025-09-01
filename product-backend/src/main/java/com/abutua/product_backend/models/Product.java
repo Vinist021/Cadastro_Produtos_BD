a2ded4a833db5f078ec4f8e8e0d19acb171a8272
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-01 16:23:45 -0300

    Identificadas tabelas e relacionamento no banco de dados

--- product-backend/src/main/java/com/abutua/product_backend/models/Product.java
+++ product-backend/src/main/java/com/abutua/product_backend/models/Product.java
@@ -1,59 +1,27 @@
 package com.abutua.product_backend.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_PRODUCT")
 public class Product {
 
+    @Id
     private int id;
     private String name;
     private String description;
     private double price;
+
+    @ManyToOne
     private Category category;
+    
     private boolean promotion;
     private boolean newProduct;
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-     public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    public Category getCategory() {
-        return category;
-    }
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-    public boolean isPromotion() {
-        return promotion;
-    }
-    public void setPromotion(boolean promotion) {
-        this.promotion = promotion;
-    }
-    public boolean isNewProduct() {
-        return newProduct;
-    }
-    public void setnewProduct(boolean newProduct) {
-        this.newProduct = newProduct;
-    }
-    
-    //Métodos Construtores
+    // Métodos Construtores
     public Product(int id, String name, String description, double price, Category category, boolean promotion, boolean newProduct) {
         this.id = id;
         this.name = name;
@@ -65,6 +33,63 @@ public class Product {
     }
 
     public Product() {
-        
+
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean isPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(boolean promotion) {
+        this.promotion = promotion;
+    }
+
+    public boolean isNewProduct() {
+        return newProduct;
+    }
+
+    public void setnewProduct(boolean newProduct) {
+        this.newProduct = newProduct;
+    }
+
 }
