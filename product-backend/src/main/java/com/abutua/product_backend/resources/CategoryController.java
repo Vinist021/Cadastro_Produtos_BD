be296d99326e5ed087ca79970c90f091bcf41314
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-02 12:03:53 -0300

    Endpoints alterados para aplicar os repositories

--- product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/CategoryController.java
@@ -3,9 +3,13 @@ package com.abutua.product_backend.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.product_backend.models.Category;
 import com.abutua.product_backend.repositories.CategoryRepository;
@@ -17,6 +21,14 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
     
+    @GetMapping("categories/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable int id) {
+        Category category = categoryRepository.findById(id)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada"));
+
+        return ResponseEntity.ok(category);
+    }
+
     @GetMapping("categories")
     public List<Category> getCategories() {
         return categoryRepository.findAll();
--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -1,9 +1,9 @@
 package com.abutua.product_backend.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,39 +16,39 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.product_backend.models.Product;
+import com.abutua.product_backend.repositories.ProductRepository;
+
 @CrossOrigin
 @RestController
 public class ProductController {
 
-    private List<Product> products = new ArrayList<>();
-    
+    @Autowired
+    private ProductRepository productRepository;
+
     @PostMapping("products")
-    public ResponseEntity<Product> save(@RequestBody Product product) {
-        product.setId(products.size()+1);
-        products.add(product);
+    public ResponseEntity<Product> save(@RequestBody Product product){
+        product = productRepository.save(product);
 
         URI location = ServletUriComponentsBuilder
-                       .fromCurrentRequest()
-                       .path("/{id}")
-                       .buildAndExpand(product.getId())
-                       .toUri();
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(product.getId())
+                    .toUri();
 
         return ResponseEntity.created(location).body(product);
     }
 
     @GetMapping("products/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id) {
-       
-        Product prod = products.stream()
-                                .filter(p -> p.getId() == id)
-                                .findFirst()
-                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+        Product product = productRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
 
-        return ResponseEntity.ok(prod);
+        return ResponseEntity.ok(product);
     }
 
     @GetMapping("products")
     public List<Product> getProducts() {
-        return products;
+        return productRepository.findAll();
     }
+
 }
